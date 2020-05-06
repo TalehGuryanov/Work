e426654b8a7d3411667ea2c9297dc45ed5f62f22
TalehGuryanov <63449924+TalehGuryanov@users.noreply.github.com>
2020-05-06 23:48:16 +0300

    Correct mistakes

--- HW3/main.js
+++ HW3/main.js
@@ -1,38 +1,34 @@
-var itemText = document.getElementsByClassName('l-tasks__item-text');
+
+var body = document.body;
+var itemText = document.querySelector('.l-tasks__item-text');
+var btnToggle = document.querySelector('.l-tasks__btn._toggle');
+var btnRemove = document.querySelector('.l-tasks__btn._remove');
+var spansArray = document.querySelectorAll('span');
 
 var func = function(num){
   if(typeof num === 'number' && num % 2 === 0){
-    itemText[0].innerText = 'Number 8 is even';
+    itemText.innerText = 'Number 8 is even';
   } else {
-    itemText[0].innerText = 'This is not a number';
-  };
+    itemText.innerText = 'This is not a number';
+  }; 
 }; func(8);
 
 console.log(itemText[0]);
 
 
-
-var body = document.body;
-
-var toggle = document.getElementsByClassName('_toggle')[0];
-
-var changeColorScheme = function(){
+var changeColorHandler = function(){
   body.classList.toggle('_color-scheme-light');
 };
 
-toggle.addEventListener('click', changeColorScheme);
+btnToggle.addEventListener('click', changeColorHandler);
 
 
-var btnRemove = document.getElementsByClassName('_remove')[0];
-
-var span = document.querySelectorAll('span');;
-
-var removeSpan = function(){
-if( body.classList.contains('_color-scheme-light')){
-span[9].remove();
-} else {
-span[0].remove();
-};
+var removeSpanHandler = function(){
+  if( body.classList.contains('_color-scheme-light')){
+    spansArray[9].remove();
+  } else {
+    spansArray[0].remove();
+  }; btnRemove.removeEventListener('click', removeSpanHandler);
 };
 
-btnRemove.addEventListener("click", removeSpan);
+btnRemove.addEventListener("click", removeSpanHandler);

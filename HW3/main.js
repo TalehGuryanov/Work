524885a44dd00bb5851206e6db4c6e0a2b53471f
TalehGuryanov <63449924+TalehGuryanov@users.noreply.github.com>
2020-05-09 11:31:46 +0300

    Added new corrects

--- HW3/main.js
+++ HW3/main.js
@@ -4,35 +4,31 @@ var itemText = document.querySelector('.l-tasks__item-text');
 var btnToggle = document.querySelector('.l-tasks__btn._toggle');
 var btnRemove = document.querySelector('.l-tasks__btn._remove');
 var spansArray = document.querySelectorAll('span');
-var changeColorHandler;
-var removeSpanHandler;
-var checkNum;
-
- checkNum = function(num){
-  if(typeof num === 'number' && num % 2 === 0){
-    itemText.innerText = 'Number ' + num + ' is even';
-  } else if (typeof num === 'number' && !(num % 2 === 0)){
-    itemText.innerText  = 'Number ' + num + ' is odd';
-  } else {
-    itemText.innerText = 'This is not a number';
-  }; 
-}; checkNum(1000);
-
-console.log(itemText[0]);
-
-
-changeColorHandler = function(){
+var changeColorHandler = function(){
   body.classList.toggle('_color-scheme-light');
 };
-
-btnToggle.addEventListener('click', changeColorHandler);
-
-removeSpanHandler = function(){
+var removeSpanHandler = function(){
   if( body.classList.contains('_color-scheme-light')){
     spansArray[spansArray.length-1].remove();
   } else {
     spansArray[0].remove();
   }; btnRemove.removeEventListener('click', removeSpanHandler);
 };
+var checkNum = function(num){
+  var type = 'odd';
 
+  if (typeof num !== 'number'){
+    return itemText.innerText = 'This is not a number';
+  }
+
+  if (num % 2 === 0){
+    type = 'even';
+  }
+
+  itemText.innerText = 'Number ' + num + ' is ' + type;
+}; 
+
+checkNum(1000);
+
+btnToggle.addEventListener('click', changeColorHandler);
 btnRemove.addEventListener("click", removeSpanHandler);

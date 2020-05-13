76d5159a9b177bf9f9bc2fa50f261ea624bd1500
TalehGuryanov <63449924+TalehGuryanov@users.noreply.github.com>
2020-05-13 21:19:41 +0300

    Correct mistakes

--- HW4/main.js
+++ HW4/main.js
@@ -1,37 +1,32 @@
 
 
 
-var universe = document.querySelector('div');
-var planetEarth = document.querySelector('img');
-var clickHandle = function(e){
-  
+var universe = document.getElementsByClassName('b-universe')[0];
+var planetEarth = document.getElementsByClassName('b-universe__img')[0];
+var clickHandler = function(e){ 
   var x = e.offsetX - planetEarth.offsetWidth/2;
   var y = e.offsetY - planetEarth.offsetHeight/2;
-
-  if ( x < 0){ x = 0};
-
-  if ( y < 0){ y = 0};
-
-  if ( x + planetEarth.offsetWidth > universe.offsetWidth){
-    x = universe.offsetWidth - planetEarth.offsetWidth
-  };
-
-  if ( y + planetEarth.offsetHeight > universe.offsetHeight){
-    y = universe.offsetHeight - planetEarth.offsetHeight
-  };
 
   if (planetEarth.style.display === 'none') {
     return planetEarth.style.display = '';
   };
-
   if (e.target.tagName.toLowerCase() === 'img') {
     return planetEarth.style.display = 'none';
   };
+  if ( x < 0){ 
+    x = 0
+  } else if(x + planetEarth.offsetWidth > universe.offsetWidth){
+    x = universe.offsetWidth - planetEarth.offsetWidth
+  };
+  if ( y < 0){ 
+    y = 0
+  } else if ( y + planetEarth.offsetHeight > universe.offsetHeight){
+    y = universe.offsetHeight - planetEarth.offsetHeight
+  };
 
   planetEarth.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-
 };
 
-universe.addEventListener('click', clickHandle);
+universe.addEventListener('click', clickHandler);
 
 

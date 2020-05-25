e24e7ac1a4643a98989d6f4f218002367bcfdde9
TalehGuryanov <63449924+TalehGuryanov@users.noreply.github.com>
2020-05-25 22:55:00 +0300

    Added HW7

---
+++ HW7/main.js
@@ -0,0 +1,67 @@
+// 1
+var array = [1, 2, 3, 4, 5];
+var newArray = array.slice();
+
+// 2
+var obj = {
+  name: 'John',
+  age: 25,
+  gender: 'male',
+};
+var newObj = Object.assign({}, obj);
+
+
+// 3
+var anotherFunction = function(x){
+  console.log(Math.sqrt(x));
+} ;
+
+var printSquareRoot = function(a, func){
+  func(a);
+};
+printSquareRoot(64, anotherFunction);
+
+
+
+// 4
+function outer() {
+
+  var array = [];
+
+  function inner(x) {
+      array.push(x);
+      console.log(array)
+      if (array.length === 5) {
+    array.length = 0;
+  }
+  };
+  return inner;
+};
+
+  var add = outer();
+  add(1);
+  add(2);
+  add(3);
+  add(4);
+  add(5);
+  add(6);
+  add(7);
+  add(8);
+  add(9);
+
+
+
+// 5
+var button = document.getElementsByTagName('button')[0];
+var clickButton = function(){
+  var count = 1;
+  return function(){
+    return button.innerHTML = 'Click count: ' + count++;
+ } 
+};
+var makeCounterHandler = clickButton();
+
+button.addEventListener('click', makeCounterHandler);
+
+
+

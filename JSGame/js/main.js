37b548e2bf3238a27a1527b8ff59781a911723e3
TalehGuryanov <63449924+TalehGuryanov@users.noreply.github.com>
2020-06-20 19:18:46 +0300

    Added part 4

--- JSGame/js/main.js
+++ JSGame/js/main.js
@@ -2,22 +2,30 @@ var cemetry = document.getElementsByClassName('b-shooter')[0];
 var aimShooter = document.getElementsByClassName('b-shooter__aim')[0];
 var aimShooterImg = document.getElementsByClassName('b-shooter__img-aim')[0];
 var ghost = document.getElementsByClassName('b-shooter__img-ghost')[0];
+var fire = document.getElementsByClassName('b-shooter__img-fire')[0];
+var delayToReset = 500;
 var clickHandler = function(e){
-  var x = e.offsetX - aimShooter.offsetWidth/2 - 50;
-  var y = e.offsetY - aimShooter.offsetHeight/2 - 100;
+
+  if(ghost.style.animationPlayState ==='paused'){
+    return
+  }
+
+  var x = e.offsetX - aimShooter.offsetWidth/2;
+  var y = e.offsetY - aimShooter.offsetHeight/2;
 
   if ( x < 0){ 
-    x = -50;
-  } else if(x + aimShooter.offsetWidth + 50 > cemetry.offsetWidth){
-    x = cemetry.offsetWidth - aimShooter.offsetWidth - 50;
+    x = 0;
+  } else if(x + aimShooter.offsetWidth > cemetry.offsetWidth){
+    x = cemetry.offsetWidth - aimShooter.offsetWidth;
   };
   if ( y < 0){ 
-    y = -100;
-  } else if ( y + aimShooter.offsetHeight + 100 > cemetry.offsetHeight){
-    y = cemetry.offsetHeight - aimShooter.offsetHeight - 100;
+    y = 0;
+  } else if ( y + aimShooter.offsetHeight > cemetry.offsetHeight){
+    y = cemetry.offsetHeight - aimShooter.offsetHeight;
   };
 
   aimShooter.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+
 };
 
 var keyDownHandler = function(e){
@@ -38,27 +46,47 @@ var keyUpHandler = function(e){
   }
 
   var rect = aimShooterImg.getBoundingClientRect();
-  var aimCenterX = rect.left + rect.width/2;
-  var aimCenterY = rect.top + rect.height/2;
+  var aimCenterX = rect.x + rect.width/2;
+  var aimCenterY = rect.y + rect.height/2;
   console.log(aimCenterX, aimCenterY);
 
+  var ghoostGoords = ghost.getBoundingClientRect();
+
+  if (
+    aimCenterX <= ghoostGoords.right - 20
+    && aimCenterX >= ghoostGoords.x + 20
+    && aimCenterY >= ghoostGoords.y - 20
+    && aimCenterY <= ghoostGoords.bottom + 20
+  ){
+    fire.style.visibility = 'visible';
+    fire.style.transitionDuration = delayToReset * 0.6 +'ms';
+    fire.style.transitionDelay = delayToReset * 0.4 + 'ms';
+    fire.style.opacity = 0;
+    ghost.style.animationPlayState = 'paused';
+    ghost.style.transitionDuration = delayToReset * 0.6 +'ms';
+    ghost.style.transitionDelay = delayToReset * 0.4 + 'ms';
+    ghost.style.opacity = 0;
+    aimShooterImg.style.display = 'none';
+  }
+
+  setTimeout(function() {
+    fire.removeAttribute('style');
+    fire.style.visibility = 'hidden';
+    aimShooterImg.style.display = '';
+    ghost.removeAttribute('style');
+    ghost.style.display = 'none';
+}, delayToReset);
 };
 
 var setRandomCoors = function(e){
 
-  var left = Math.floor(Math.random() * 1000);
-  var top = Math.floor(Math.random() * 1000);
+  if (ghost.style.animationPlayState ==='paused') {
+    return;
+  }
 
-  if ( left < 0){ 
-    left = 0;
-  } else if(left + ghost.offsetWidth > cemetry.offsetWidth){
-    left = cemetry.offsetWidth - ghost.offsetWidth;
-  };
-  if ( top < 0){ 
-    top = 0;
-  } else if ( top + ghost.offsetHeight > cemetry.offsetHeight){
-    top = cemetry.offsetHeight - ghost.offsetHeight;
-  };
+  var left = Math.floor(Math.random() * (cemetry.offsetWidth - ghost.offsetWidth));
+  var top = Math.floor(Math.random() * (cemetry.offsetHeight - ghost.offsetHeight));
+
 
   ghost.style.left = left + 'px';
   ghost.style.top = top + 'px';
@@ -66,6 +94,7 @@ var setRandomCoors = function(e){
   if(ghost.style.display = 'none'){
        ghost.style.display = '';
   }
+
 };
 
 

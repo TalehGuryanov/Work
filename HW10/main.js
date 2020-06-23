83ba4b3d7c3492d610534cbad56fff4a8f6f675f
TalehGuryanov <63449924+TalehGuryanov@users.noreply.github.com>
2020-06-23 17:34:59 +0300

    Added users list

--- HW10/main.js
+++ HW10/main.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const container = document.getElementsByClassName('container')[0];
+let maleUsers = [];
+let femaleUsers = [];
 const requestForUsers = function(success, error, url){
   const xhr = new XMLHttpRequest();
   xhr.open('GET', "https://randomuser.me/api/?results=3&gender" + url);
@@ -23,6 +26,10 @@ const requestForUsers = function(success, error, url){
 requestForUsers(
   function(x){
     console.log(x);
+    maleUsers = x.results;
+    if(maleUsers.length && femaleUsers.length){
+      getUsersData()
+    }
   },
   function(y){
     console.log(y);
@@ -33,6 +40,10 @@ requestForUsers(
 requestForUsers(
   function(x){
     console.log(x);
+    femaleUsers = x.results;
+    if(maleUsers.length && femaleUsers.length){
+      getUsersData()
+    }
   },
   function(y){
     console.log(y);
@@ -41,3 +52,28 @@ requestForUsers(
 );
 
 
+let getUsersData = function(){
+  let allUsers = maleUsers.concat(femaleUsers);
+  const ul = document.createElement('ul');
+  ul.classList.add('inner_list')
+
+  allUsers.forEach(function(item){
+    const li = document.createElement('li');
+
+    li.classList.add('list_item');
+
+    li.innerHTML = `
+      <div class="list_content">
+      <div class="user_img">
+        <img class="img" src="${item.picture.large}" alt="User Image">
+      </div>
+      <div class="user_data">
+        <h4>Hi, My name is</h4>
+        <p class="user_name"> ${item.name.first} ${item.name.last}</p>
+        <a class="user_email" href="#">${item.email}</a>
+      </div>
+    </div>`;
+    ul.appendChild(li);
+  })
+  container.appendChild(ul);
+}

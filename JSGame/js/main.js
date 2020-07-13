166799810bfa3d04e3178f076511b11df2ba8ed1
TalehGuryanov <63449924+TalehGuryanov@users.noreply.github.com>
2020-07-14 00:19:45 +0300

    Added game over function

--- JSGame/js/main.js
+++ JSGame/js/main.js
@@ -3,12 +3,19 @@ var aimShooter = document.getElementsByClassName('b-shooter__aim')[0];
 var aimShooterImg = document.getElementsByClassName('b-shooter__img-aim')[0];
 var ghost = document.getElementsByClassName('b-shooter__img-ghost')[0];
 var fire = document.getElementsByClassName('b-shooter__img-fire')[0];
+var progressIcon = document.getElementsByClassName('b-shooter__progress-icon');
+var healthIcon = document.getElementsByClassName('b-shooter__health-icon');
+var healthIconBar = document.getElementsByClassName('b-shooter__health')[0];
 var delayToReset = 500;
+var isGameOver = false;
+var shooterGameOver = document.querySelector('.b-shooter__game-over');
+var shooterGameOverTitle = document.getElementsByClassName('.b-shooter__game-over-title')[0];
+
 var clickHandler = function(e){
 
-  if(ghost.style.animationPlayState ==='paused'){
+  if(ghost.style.animationPlayState ==='paused' || isGameOver == true){
     return
-  }
+  };
 
   var x = e.offsetX - aimShooter.offsetWidth/2;
   var y = e.offsetY - aimShooter.offsetHeight/2;
@@ -32,8 +39,6 @@ var keyDownHandler = function(e){
 
   if (e.code === 'Enter') {
     aimShooterImg.style.transform = 'scale(.9)';
-  } else {
-    return;
   }
 };
 
@@ -41,8 +46,6 @@ var keyUpHandler = function(e){
 
   if (e.code === 'Enter') {
     aimShooterImg.style.transform = '';
-  } else {
-    return;
   }
 
   var rect = aimShooterImg.getBoundingClientRect();
@@ -67,7 +70,9 @@ var keyUpHandler = function(e){
     ghost.style.transitionDelay = delayToReset * 0.4 + 'ms';
     ghost.style.opacity = 0;
     aimShooterImg.style.display = 'none';
-  }
+
+    markProgress();
+  } else markLifeStatus();
 
   setTimeout(function() {
     fire.removeAttribute('style');
@@ -75,30 +80,66 @@ var keyUpHandler = function(e){
     aimShooterImg.style.display = '';
     ghost.removeAttribute('style');
     ghost.style.display = 'none';
-}, delayToReset);
+  }, delayToReset);
 };
 
 var setRandomCoors = function(e){
 
-  if (ghost.style.animationPlayState ==='paused') {
-    return;
-  }
+  var left = Math.floor(Math.random() * (cemetry.offsetWidth - ghost.offsetWidth) +1);
+  var top = Math.floor(Math.random() * (cemetry.offsetHeight - ghost.offsetHeight) +1);
 
-  var left = Math.floor(Math.random() * (cemetry.offsetWidth - ghost.offsetWidth));
-  var top = Math.floor(Math.random() * (cemetry.offsetHeight - ghost.offsetHeight));
-
-
+  console.log(left, top)
   ghost.style.left = left + 'px';
   ghost.style.top = top + 'px';
 
-  if(ghost.style.display = 'none'){
-       ghost.style.display = '';
-  }
+};
 
+function markProgress(){
+  
+  for( let i = 0; i < progressIcon.length; i++){
+    if(!progressIcon[i].classList.contains('_score')){
+      progressIcon[i].classList.add('_score');
+      console.log(isGameOver)
+
+      if(i === progressIcon.length-1){
+        isGameOver = true;
+        shooterGameOver.classList.add('_win');
+        shooterGameOverTitle.innerHTML = '<h2>You Lose</h2>'
+      }
+      break
+    } 
+  }
+};
+
+function markLifeStatus(){
+
+  if(healthIconBar.classList.contains('_loss-bar')){
+    isGameOver = true;
+    return
+  };
+
+  for( let i = 0; i < healthIcon.length; i++){
+    if(!healthIcon[i].classList.contains('_loss')){
+      healthIcon[i].classList.add('_loss');
+
+      if(i === healthIcon.length-1){
+        healthIconBar.classList.add('_loss-bar')
+        shooterGameOver.classList.add('_lose');
+      }
+      break
+    } 
+  }
 };
 
 
-var setRandomCoorsInterval = setInterval(setRandomCoors, 3000);
+var setRandomCoorsInterval = setInterval(function(){
+  if (ghost.style.animationPlayState ==='paused') {
+    return;
+  } else if (ghost.style.display == 'none'){
+    ghost.style.display = '';
+    setRandomCoors();
+  }
+}, 3000);
 
 document.addEventListener('keydown', keyDownHandler);
 document.addEventListener('keyup', keyUpHandler);
